444895a071eddcd0143f168a9eac2cd7477fa603
codingjordi <jordisaperasc@gmail.com>
2024-12-16 18:08:33 +0100

    mysql-connection-fix

---
+++ server/app.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import cors from 'cors'
+import { PORT } from './config.js'
+
+import indexRoutes from './routes/index.routes.js'
+import tasksRoutes from './routes/tasks.routes.js'
+
+const app = express()
+app.use(function(req, res, next) {
+    res.removeHeader('X-Powered-By');
+    next();
+  }
+);
+
+app.use(cors())
+app.use(express.json())
+
+app.use(indexRoutes) 
+app.use(tasksRoutes)
+
+export default app; 
--- server/index.js
+++ server/index.js
@@ -1,22 +1,5 @@
-import express from 'express'
-import cors from 'cors'
-import { PORT } from './config.js'
-
-import indexRoutes from './routes/index.routes.js'
-import tasksRoutes from './routes/tasks.routes.js'
-
-const app = express()
-app.use(function(req, res, next) {
-    res.removeHeader('X-Powered-By');
-    next();
-  }
-);
-
-app.use(cors())
-app.use(express.json())
-
-app.use(indexRoutes) 
-app.use(tasksRoutes)
+import app from "./app.js";
+import { PORT } from "./config.js";
 
 app.listen(PORT, () => {
     console.log('Listening on port: ' + PORT);
